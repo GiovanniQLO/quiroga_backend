31ea2d68e3188739a9f24c45c3134557f98a8a76
GiovanniQLO <gpalenciaosorio@gmail.com>
2025-12-23 14:27:59 -0500

    chore(main): added main class.

---
+++ src/test/java/com/quiroga/backend/QuirogaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.quiroga.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuirogaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
